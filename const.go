ae96ea3c840f00dcf6e20b21177f9fb914f910c8
Alonza0314 <alonza.tu@gmail.com>
2026-01-20 13:48:25 +0000

    feat: frush base

---
+++ const.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	CMD_START = "frush > "
+	CMD_EXIT = "exit"
+)
+
+const (
+	SYSTEM_HINT_CTRL_C_EXIT = "If you want to exit, please type 'exit'"
+	SYSTEM_HINT_UNKNOWN_CMD = "Unknown command: %s"
+)
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/chzyer/readline"
+)
 
 func printFrush() {
 	fmt.Print(`=============== Welcome to use frush! ===============
@@ -10,9 +15,39 @@ func printFrush() {
 ======██╔══╝  ██╔══██╗██║   ██║╚════██║██╔══██║======
 ======██║     ██║  ██║╚██████╔╝███████║██║  ██║======
 ======╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝======
-=====================================================`)
+=====================================================
+`)
 }
 
 func main() {
 	printFrush()
+
+	rl, err := readline.New(CMD_START)
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			if err == readline.ErrInterrupt {
+				fmt.Println(SYSTEM_HINT_CTRL_C_EXIT)
+				continue
+			}
+			panic(err)
+		}
+
+		cmds := strings.Fields(line)
+		if len(cmds) == 0 {
+			continue
+		}
+
+		switch cmds[0] {
+		case CMD_EXIT:
+			return
+		default:
+			fmt.Println(fmt.Sprintf(SYSTEM_HINT_UNKNOWN_CMD, cmds[0]))
+		}
+	}
 }
