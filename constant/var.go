c049da00297f59403658876901e90a388f0d3d7f
Alonza0314 <alonza.tu@gmail.com>
2026-01-20 14:54:34 +0000

    feat: config template

---
+++ constant/var.go
@@ -0,0 +1,8 @@
+package constant
+
+var (
+	TEMPLATE_SUBSCRIBER_YAML      = "./config/subscriber_template.yaml"
+	TEMPLATE_CONSOLE_ACCOUNT_YAML = "./config/console_account_template.yaml"
+	TEMPLATE_GNB_YAML             = "./config/gnb_template.yaml"
+	TEMPLATE_UE_YAML              = "./config/ue_template.yaml"
+)
